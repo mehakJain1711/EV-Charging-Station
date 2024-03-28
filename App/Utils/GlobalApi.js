60203754b924387f885725d994bb4ecd6c1d8aaa
shubhi2092 <104774342+shubhi2092@users.noreply.github.com>
2024-03-28 23:27:59 +0530

    Global Api

--- App/Utils/GlobalApi.js
+++ App/Utils/GlobalApi.js
@@ -1,62 +1,23 @@
-// const BASE_URL="https://api.geocode.earth/v1/autocomplete";
-// const API_KEY="ge-3e05aee5a2de2f61"
+import axios from "axios";
+const BASE_URL = "https://places.googleapis.com/v1/places:searchNearby";
+const API_KEY = "YOUR_API-KEY";
 
-// import axios from 'axios';
+const config = {
+  Headers: {
+    "content-type": "application/json",
+    "X-Goog-Api-Key": API_KEY,
+    "X-Goog-FieldMask": [
+      "places.dispayName",
+      "places.formattedAddress",
+      "places.formattedAddress",
+      "places.location",
+      "places.evChargerOption",
+      "places.photos",
+    ],
+  },
+};
 
-// export default function SearchBar({ lat, lon }) {
-//     const [searchTerm, setSearchTerm] = useState('');
-//     const [searchResults, setSearchResults] = useState([]);
-  
-//     const handleSearch = async () => {
-//       const apiKey = "ge-3e05aee5a2de2f61";
-//       const options = {
-//         method: 'GET',
-//         url: 'https://api.geocode.earth/v1/autocomplete',
-//         params: {
-//           api_key: apiKey,
-//           'focus.point.lat': lat ?? 20.59,
-//           'focus.point.lon': lon ?? 78.96,
-//           text: searchTerm
-//         },
-//       };
-  
-//       try {
-//         const response = await axios.request(options);
-//         setSearchResults(response.data.features); // Set the search results state variable with the response data
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-  
-//     useEffect(() => {
-//       // You can include additional logic here if needed
-//     }, [searchResults]);
-  
-//     const handleLocationSelect = (location) => {
-//       console.log("Selected Location Latitude:", location?.geometry?.coordinates[1]);
-//       console.log("Selected Location Longitude:", location?.geometry?.coordinates[0]);
-//       // Additional logic if needed
-//     };
-  
-//     return (
-//       <View style={styles.container}>
-//         <TextInput
-//           style={styles.input}
-//           placeholder='Search EV Charging Station'
-//           value={searchTerm}
-//           onChangeText={setSearchTerm}
-//         />
-//         <Button title="Search" onPress={handleSearch} />
-//         {searchResults && searchResults.length ? searchResults.map(function (location) {
-//           return (
-//             <TouchableOpacity key={location.id} onPress={() => handleLocationSelect(location)}>
-//               <Text>
-//                 {location?.properties?.label}
-//               </Text>
-//             </TouchableOpacity>
-//           );
-//         }) : <Text>No results found!</Text>}
-//       </View>
-//     );
-//   }
-  
+const NewNearByPlaces = (data) => axios.post(BASE_URL, data, config);
+export default {
+  NewNearByPlaces,
+};
