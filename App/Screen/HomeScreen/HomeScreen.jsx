ad7d0e14f324184e00632205e2300f8225dfd239
mehakJain1711 <mehakjain1711@gmail.com>
2024-03-28 23:17:51 +0530

    MERGE

--- App/Screen/HomeScreen/HomeScreen.jsx
+++ App/Screen/HomeScreen/HomeScreen.jsx
@@ -1,10 +1,35 @@
 import { StyleSheet,View, Text } from 'react-native';
-import React from 'react'
+import React, { useContext } from 'react'
 import AppMapView from './AppMapView'
 import Header from './Header'
 import SearchBar from './searchBar';
-
+import { UserLocationContext } from '../../Context/UserLocationContext';
+import GlobalApi from '../../Utils/GlobalApi'
 export default function HomeScreen() {
+
+  const{location,setLocation}=useContext(UserLocationContext);
+
+  useEffect(()=>{
+    location&&GetNearByPlace();
+  },[location])
+  const GetNearByPlace=()=>{
+    const data={
+      "includedTypes": ["electric_vehicle_charging_station"],
+      "maxResultCount": 10,
+      "locationRestriction": {
+      "circle": {
+      "center": {
+          "latitude": location?.latitude,
+          "longitude": location?.longitude
+        },
+        "radius": 5000.0
+      }
+    }
+  }
+    GlobalApi.NewNearByPlace(data).then(resp=>{
+      console.log(resp.data);
+    })
+  }
   return (
     <View>
       <View style={styles.headerContainer}>
